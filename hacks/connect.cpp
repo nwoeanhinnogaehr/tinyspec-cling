42df73c7da5538ae9f313f620530e16e25329a1e
Noah Weninger <nweninge@ualberta.ca>
2020-03-21 16:32:18 -0700

    more examples

---
+++ hacks/connect.cpp
@@ -0,0 +1,25 @@
+// feed back renoise through tinyspec
+system("jack_connect tinyspec_cmd1:out0 renoise:input_01_left");
+system("jack_connect tinyspec_cmd1:out1 renoise:input_01_right");
+system("jack_connect renoise:output_02_left tinyspec_cmd1:in0");
+system("jack_connect renoise:output_02_right tinyspec_cmd1:in1");
+
+// detour pulse through tinyspec
+system("jack_disconnect \"PulseAudio JACK Sink:front-left\" system:playback_1");
+system("jack_disconnect \"PulseAudio JACK Sink:front-right\" system:playback_2");
+system("jack_connect tinyspec_cmd1:out0 system:playback_1");
+system("jack_connect tinyspec_cmd1:out1 system:playback_2");
+system("jack_connect \"PulseAudio JACK Sink:front-left\" tinyspec_cmd1:in0");
+system("jack_connect \"PulseAudio JACK Sink:front-right\" tinyspec_cmd1:in1");
+
+// tinyspec to system
+system("jack_disconnect tinyspec_cmd1:out0 system:playback_1");
+system("jack_disconnect tinyspec_cmd1:out1 system:playback_2");
+
+// tinyspec to renoise
+system("jack_connect tinyspec_cmd1:out0 renoise:input_01_left");
+system("jack_connect tinyspec_cmd1:out1 renoise:input_01_right");
+
+// Revert pulse to normal
+system("jack_connect \"PulseAudio JACK Sink:front-left\" system:playback_1");
+system("jack_connect \"PulseAudio JACK Sink:front-right\" system:playback_2");
---
+++ hacks/tidal.cpp
@@ -0,0 +1,25 @@
+/* Example of controlling tinyspec with tidalcycles via OSC
+   Adapted from simple-pitchshift.cpp
+
+tinyspecTarget = OSCTarget {oName = "Tinyspec", oAddress = "127.0.0.1", oPort = 44101, oPath = "/shift", oShape = Just [("shift", Just $ VF 0)], oLatency = 0.02, oPreamble = [], oTimestamp = MessageStamp}
+tidal <- startTidal tinyspecTarget defaultConfig
+let p = streamReplace tidal
+let shift = pF "shift"
+p 1 $ sometimes rev $ shift "0.5 0.75 1 1.25 1.5 1.75 2"
+
+*/
+
+set_process_fn([&](WaveBuf &in, WaveBuf &out, int n, double t){
+    static float shift = 1;
+    for (auto msg : osc_recv(44101, t, "/shift")) {
+        osc_get(*msg, shift);
+        if (shift < 0.1) shift = 0.1;
+    }
+    window_hann(in);
+    out.resize(2, n/shift);
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = j/shift; k < (j+1)/shift; k++)
+                out[i][k] = in[i][j];
+    next_hop_ratio(8192,0.5/shift);
+});
