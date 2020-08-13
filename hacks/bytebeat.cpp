e087930c65e2a73a4afcc3d05772fe9d966ea845
Noah Weninger <nweninge@ualberta.ca>
2020-08-12 19:47:31 -0700

    Finally a bytebeat example! (floaty)

---
+++ hacks/bytebeat.cpp
@@ -0,0 +1,12 @@
+// Simple bytebeat synth achieved by setting both frame size and hop to 1 sample.
+set_num_channels(0,1);
+system("jack_connect " CLIENT_NAME ":out0 system:playback_1");
+system("jack_connect " CLIENT_NAME ":out0 system:playback_2");
+
+set_process_fn([&](WaveBuf&, WaveBuf& out, int, double ts){
+    double t = ts*2000;
+    int y = t;
+    int s = int(fmod(t,(1+(t/(1.0+(y&(y>>9^y>>11)))))));
+    out[0][0] = s%256/128.0-1;
+    next_hop_samples(1,1);
+});
